e38e69356854d4d7c0dcf7463f8404977d744bb3
R. Keoni Garcia <keoni@Tauriel.local>
2015-08-30 12:36:29 -0500

    refactor RGBATileProgram into Program.py modified vertex shader to have Z as input+attribute for layering

--- py/cspov/view/MapWidget.py
+++ py/cspov/view/MapWidget.py
@@ -17,23 +17,20 @@ REQUIRES
 :copyright: 2014 by University of Wisconsin Regents, see AUTHORS for more details
 :license: GPLv3, see LICENSE for more details
 """
-from OpenGL.GL import *
 # from PyQt4.QtCore import *
 # from PyQt4.QtOpenGL import QGLWidget, QGLFormat
 from vispy import app, gloo
 import numpy as np
 import scipy.misc as spm
-from cspov.view.Layer import TestTileLayer, Layer
-from cspov.common import MAX_EXCURSION_Y, MAX_EXCURSION_X, box
-from vispy.util.transforms import perspective, translate, rotate, ortho
-from vispy.io import read_mesh, load_data_file, load_crate
-from vispy.geometry import create_plane
+from vispy.util.transforms import translate, rotate, ortho
+
+from cspov.common import box
+from cspov.view.Program import RGBATileProgram
 
 __author__ = 'rayg'
 __docformat__ = 'reStructuredText'
 
-import os, sys
-import logging, unittest, argparse
+import logging
 
 LOG = logging.getLogger(__name__)
 
@@ -198,37 +195,6 @@ class Animating(MapWidgetActivity):
     :return:
     """
 
-# basic vertex shader for projection
-VERT_CODE = """
-uniform   mat4 u_model;
-uniform   mat4 u_view;
-uniform   mat4 u_projection;
-
-attribute vec3 a_position;
-attribute vec2 a_texcoord;
-
-varying vec2 v_texcoord;
-
-void main()
-{
-    v_texcoord = a_texcoord;
-    gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
-    //gl_Position = vec4(a_position,1.0);
-}
-"""
-
-# simple fragment shader for putting a slightly modified texture onto a set of polys
-FRAG_CODE = """
-uniform sampler2D u_texture;
-varying vec2 v_texcoord;
-
-void main()
-{
-    float ty = v_texcoord.y;
-    float tx = v_texcoord.x;
-    gl_FragColor = texture2D(u_texture, vec2(tx, ty));
-}
-"""
 
 # from imshow_cuts.py
 image_vertex = """
@@ -287,73 +253,7 @@ void main()
 #         self.image['image'] = I.astype('float32')
 #         self.image['image'].interpolation = 'linear'
 
-class RGBATileProgram(object):
-    program = None
-    image = None
-    world_box = None
-    faces = None
 
-    def __init__(self, world_box=None, image=None, image_box=None):
-        super(RGBATileProgram, self).__init__()
-        self.program = gloo.Program(VERT_CODE, FRAG_CODE)
-        if image is not None:
-            self.set_texture(image, image_box)
-        if world_box is not None:
-            self.set_world(world_box)
-
-    def set_world(self, world_box):
-        # get the geometry queued
-        vtnc, faces, outline = plane = create_plane(width=world_box.r-world_box.l,
-                                             height=world_box.t-world_box.b,
-                                             direction='+z')
-
-        verts = np.array([q[0] for q in vtnc])
-        # translate the vertices
-        verts[:,0] -= (world_box.l + world_box.r)/2.0
-        verts[:,1] -= (world_box.b + world_box.t)/2.0
-        texcoords = np.array([q[1] for q in vtnc])
-        normals = np.array([q[2] for q in vtnc])
-        colors = np.array([q[3] for q in vtnc])
-        faces_buffer = gloo.IndexBuffer(faces.astype(np.uint16))
-        # print("V:", verts, len(verts))
-        # print("T:", texcoords, len(texcoords))
-        # print("N:", normals, len(normals))
-        # print("C:", colors, len(colors))
-        # print("F:", faces, len(faces))
-        # print("O:", outline, len(outline))
-        # print("T:", texcoords, len(texcoords))
-
-        self.program['a_position'] = gloo.VertexBuffer(verts)
-        self.program['a_texcoord'] = gloo.VertexBuffer(texcoords)
-        self.faces = faces_buffer
-        self.world_box = world_box
-
-
-    def set_texture(self, image, image_box=None):
-        if isinstance(image, str) and image == 'crate':
-            image = load_crate()
-        if image_box is not None:
-            self.image = image = image[image_box.b:image_box.t, image_box.l:image_box.r]
-            print("clipping")
-        else:
-            self.image = image
-        # get the texture queued
-        self.program['u_texture'] = gloo.Texture2D(image)
-
-
-    def draw(self):
-        assert(self.image is not None)
-        assert(self.world_box is not None)
-        self.program.draw('triangles', self.faces)
-
-
-    def update_mvp(self, model=None, view=None, projection=None):
-        if model is not None:
-            self.program['u_model'] = model
-        if view is not None:
-            self.program['u_view'] = view
-        if projection is not None:
-            self.program['u_projection'] = projection
 
 
 
@@ -463,14 +363,14 @@ class CspovMainMapWidget(app.Canvas):
         self.view = translate((0, 0, -5))
         self.model = np.eye(4, dtype=np.float32)
         self.projection = np.eye(4, dtype=np.float32)
-        self._testtile.update_mvp(self.model, self.view, self.projection)
+        self._testtile.set_mvp(self.model, self.view, self.projection)
 
     def update_transforms(self, event):
         self.theta += .1
         self.phi += .1
         self.model = np.dot(rotate(self.theta, (0, 0, 1)),
                             rotate(self.phi, (0, 1, 0)))
-        self._testtile.update_mvp(self.model)
+        self._testtile.set_mvp(self.model)
         self.update()
 
     def on_resize(self, event):
@@ -491,7 +391,7 @@ class CspovMainMapWidget(app.Canvas):
             vp.b, vp.t,
             -10, 10
         )
-        self._testtile.update_mvp(projection=self.projection)
+        self._testtile.set_mvp(projection=self.projection)
 
     def on_draw(self, event):
         gloo.clear()
---
+++ py/cspov/view/Program.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.py
+~~~
+
+PURPOSE
+gloo.Program wrappers for different purposes such as tile drawing
+
+REFERENCES
+
+
+REQUIRES
+
+
+:author: R.K.Garcia <rayg@ssec.wisc.edu>
+:copyright: 2015 by University of Wisconsin Regents, see AUTHORS for more details
+:license: GPLv3, see LICENSE for more details
+"""
+
+import numpy as np
+from vispy import gloo
+from vispy.geometry import create_plane
+from vispy.io import load_crate
+
+# FIXME: use uniform float _z
+
+VERT_CODE = """
+uniform   mat4 u_model;
+uniform   mat4 u_view;
+uniform   mat4 u_projection;
+uniform  float u_z;
+
+attribute vec3 a_position;
+attribute vec2 a_texcoord;
+
+varying vec2 v_texcoord;
+
+void main()
+{
+    v_texcoord = a_texcoord;
+    //gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
+    gl_Position = u_projection * u_view * u_model * vec4(a_position.xy,u_z,1.0);
+    //gl_Position = vec4(a_position,1.0);
+}
+"""
+
+FRAG_CODE = """
+uniform sampler2D u_texture;
+varying vec2 v_texcoord;
+
+void main()
+{
+    float ty = v_texcoord.y;
+    float tx = v_texcoord.x;
+    gl_FragColor = texture2D(u_texture, vec2(tx, ty));
+}
+"""
+
+
+class RGBATileProgram(object):
+    program = None
+    image = None
+    world_box = None
+    faces = None
+    z = 0.0
+
+    def __init__(self, world_box=None, image=None, image_box=None):
+        super(RGBATileProgram, self).__init__()
+        self.program = gloo.Program(VERT_CODE, FRAG_CODE)
+        if image is not None:
+            self.set_texture(image, image_box)
+        if world_box is not None:
+            self.set_world(world_box)
+
+    def set_world(self, world_box):
+        # get the geometry queued
+        vtnc, faces, outline = create_plane(width=world_box.r-world_box.l,
+                                             height=world_box.t-world_box.b,
+                                             direction='+z')
+
+        verts = np.array([q[0] for q in vtnc])
+        # translate the vertices
+        verts[:,0] -= (world_box.l + world_box.r)/2.0
+        verts[:,1] -= (world_box.b + world_box.t)/2.0
+        texcoords = np.array([q[1] for q in vtnc])
+        # normals = np.array([q[2] for q in vtnc])
+        # colors = np.array([q[3] for q in vtnc])
+        faces_buffer = gloo.IndexBuffer(faces.astype(np.uint16))
+        # print("V:", verts, len(verts))
+        # print("T:", texcoords, len(texcoords))
+        # print("N:", normals, len(normals))
+        # print("C:", colors, len(colors))
+        # print("F:", faces, len(faces))
+        # print("O:", outline, len(outline))
+        # print("T:", texcoords, len(texcoords))
+
+        self.program['a_position'] = gloo.VertexBuffer(verts)
+        self.program['a_texcoord'] = gloo.VertexBuffer(texcoords)
+        self.faces = faces_buffer
+        self.world_box = world_box
+
+    def set_texture(self, image, image_box=None):
+        if isinstance(image, str) and image == 'crate':
+            image = load_crate()
+        if image_box is not None:
+            self.image = image = image[image_box.b:image_box.t, image_box.l:image_box.r]
+            print("clipping")
+        else:
+            self.image = image
+        # get the texture queued
+        self.program['u_texture'] = gloo.Texture2D(image)
+
+
+    def set_mvp(self, model=None, view=None, projection=None):
+        if model is not None:
+            self.program['u_model'] = model
+        if view is not None:
+            self.program['u_view'] = view
+        if projection is not None:
+            self.program['u_projection'] = projection
+
+
+    def draw(self):
+        assert(self.image is not None)
+        assert(self.world_box is not None)
+        self.program['u_z'] = self.z
+        self.program.draw('triangles', self.faces)
