a1bfcac67b875032e3fa457f80ed52dc09754978
R. Keoni Garcia <keoni@Tauriel.local>
2015-08-31 10:40:33 -0500

    clean up and flesh out layerstack

--- py/cspov/view/Layer.py
+++ py/cspov/view/Layer.py
@@ -19,11 +19,9 @@ REQUIRES
 """
 import os, sys
 import logging, unittest, argparse
-from OpenGL.GL import *
-from PIL import Image
 import numpy as np
-
 import scipy.misc as spm
+from PyQt4.QtCore import QObject, pyqtSignal
 from cspov.common import pnt, rez, MAX_EXCURSION_Y, MAX_EXCURSION_X, MercatorTileCalc, WORLD_EXTENT_BOX, \
     DEFAULT_TILE_HEIGHT, DEFAULT_TILE_WIDTH, box
 from cspov.view.Program import GlooRGBTile
@@ -36,7 +34,7 @@ LOG = logging.getLogger(__name__)
 
 
 
-class Layer(object):
+class Layer(QObject):
     """
     A Layer
     - has one or more representations available to immediately draw
@@ -45,6 +43,9 @@ class Layer(object):
     - typically will cache a "coarsest" single-tile representation for zoom-out events (preferred for fast=True paint calls)
     - can have probes attached which operate primarily on the science representation
     """
+    def __init__(self):
+        super(Layer, self).__init__()
+    
     def get_z(self):
         pass
 
@@ -60,6 +61,14 @@ class Layer(object):
         pass
 
     alpha = property(get_alpha, set_alpha)
+
+    def get_name(self):
+        pass
+
+    def set_name(self, new_alpha):
+        pass
+
+    name = property(get_alpha, set_alpha)
 
     def paint(self, geom, mvp, fast=False, **kwargs):
         """
@@ -103,31 +112,35 @@ class Layer(object):
 
 
 
-class LayerStack(object):
-    layers = None
+class LayerStack(QObject):
+    """
+    The master layer stack used by MapWidget; contains signals and can be controlled by GUI or script
+
+    """
+    layerStackDidChangeOrder = pyqtSignal(tuple)  # new order as ordinals e.g. (0, 2, 1, 3)
+
+    _layerlist = None
 
     def __init__(self, init_layers=None):
         super(LayerStack, self).__init__()
-        self.layers = list(init_layers or [])
+        self._layerlist = list(init_layers or [])
 
     def __iter__(self):
-        for level,layer in enumerate(reversed(self.layers)):
+        for level,layer in enumerate(reversed(self._layerlist)):
             layer.z = float(level)
             yield layer
 
+    def __getitem__(self, item):
+        return self._layerlist[item]
+
+    def __len__(self):
+        return len(self._layerlist)
+
+    @property
+    def top(self):
+        return self._layerlist[0] if self._layerlist else None
 
 
-
-TEST_COLORS = [
-    (1.,1.,1.),
-    (0.1,0.1,0.1),
-    (0.,0.,1.),
-    (0.,1.,0.),
-    (0.,1.,1.),
-    (1.,0.,0.),
-    (1.,0.,1.),
-    (1.,1.,0.),
-]
 
 
 class MercatorTiffTileLayer(Layer):
@@ -197,202 +210,6 @@ class BackgroundRGBWorldTiles(Layer):
                 t.set_mvp(model=self.model, view=self.view)
 
 
-
-
-class FlatFileTileSet(object):
-    """
-    A lazy-loaded image which can be mapped to a texture buffer and drawn on a polygon
-    Represents a single x-y coordinate range at a single level of detail
-    Will map data into GL texture memory for rendering
-
-    Tiles have
-    - extents in the mercator-meters space
-    - a texture map
-    - later: a shader which may be shared
-    """
-    data = None
-    path = None
-    _active = None     # {(y,x): (texture-id, texture-box), ...}
-    _calc = None   # calculator
-
-    def __init__(self, path, element_dtype, shape, zero_point, pixel_rez, tile_shape=(
-    DEFAULT_TILE_HEIGHT, DEFAULT_TILE_WIDTH)):
-        """
-        map the file as read-only
-        chop it into data tiles
-        tiles are set up such that tile (0,0) starts with zero_point and goes up and to the r
-
-        """
-        super(FlatFileTileSet, self).__init__()
-        self.data = np.memmap(path, element_dtype, 'r', shape=shape)
-        self._calc = MercatorTileCalc(name=path, pixel_shape=shape, zero_point=zero_point, pixel_rez=pixel_rez, tile_shape=tile_shape)
-        self.path = path
-        self._active = {}
-
-
-    def __getitem__(self, tileyx):
-        try:
-            return self._active[tuple(tileyx)]
-        except KeyError as unavailable:
-            return self.activate(tileyx)
-
-
-    def activate(self, tile_yx, ufunc=None):
-        """
-        load a texture map from this data, optionally filtering data with a numpy ufunc
-        return (texture-id, box)
-        """
-        # FUTURE: implement using glGenBuffers() and use a shader to render
-        # We want to push buffers of data into GL, and use GLSL to determine the color/transparency of the data.
-        # See VisPy for tools and techniques.
-        # For now, alpha test with plain old textures
-
-        texid = glGenTextures(1)
-
-        # offset within the array
-        th,tw = self._calc.tile_shape
-        # tile_y, tile_x = tile_yx
-        # ys = (tile_y*th)*self._calc.zero_point[0]
-        # xs = (tile_x*tw)*self._calc.zero_point[1]
-        # ye = ys + th
-        # xe = xs + tw
-        # assert(xe<=self.data.shape[1])
-        # assert(ye<=self.data.shape[0])
-        # tiledata = self.data[ys:ye, xs:xe]
-        # prepare a tile of data for OpenGL
-        tiledata = self._calc.tile_pixels(self.data, *tile_yx)
-        tile = np.require(tiledata, dtype=np.float32, requirements=['C_CONTIGUOUS', 'ALIGNED'])
-        del tiledata
-
-        # FIXME: temporarily require that textures aren't odd sizes
-        txbox = box(b=0, l=0, t=th, r=tw)
-
-        glBindTexture(GL_TEXTURE_BUFFER, texid)
-        LOD = 0
-        glTexSubImage2D(GL_TEXTURE_2D, LOD, 0,0, tw,th, GL_RED, GL_FLOAT, tile.data)
-
-        nfo = (texid, txbox)
-        self._active[tile_yx] = nfo
-        return nfo
-
-
-        # # start working with this buffer
-        # glBindBuffer(GL_COPY_WRITE_BUFFER, buffer_id)
-        # # allocate memory for it
-        # glBufferData(GL_COPY_WRITE_BUFFER, , size, )
-        # # borrow a writable pointer
-        # pgl = glMapBuffer(, )
-        # # make a numpy ndarray wrapper that targets that memory location
-        #
-        # # copy the data from the memory mapped file
-        # # if there's not enough data in the file, fill with NaNs first
-        # if npslice.shape != npgl.shape:
-        #     npgl[:] = np.nan(0)
-        #
-        # if not transform_func:
-        #     # straight copy
-        #     np.copyto(npgl, npslice, casting='unsafe')
-        # else:
-        #     # copy through transform function
-        #
-        # # let GL push it to the GPU
-        #glUnmapBuffer(GL_COPY_WRITE_BUFFER)
-        # also see glTexBuffer()
-
-
-    def deactivate(self, tile_y, tile_x):
-        """
-        release a given
-        """
-        k = (tile_y, tile_x)
-        t = self._active[k]
-        del self._active[k]
-
-        glDeleteTextures([t])
-
-
-class TextureTileLayer(Layer):
-    """
-    A layer represented as an array of quads with textures on them
-    """
-    pass
-
-
-class ColormapTiles(TextureTileLayer):
-    """
-    Mercator-projected geographic layer with one or more levels of detail.
-    Coarsest level of detail (1° typically) is always expected to be available for fast zoom.
-
-    """
-    _tileset = None
-    _image = None
-    _shape = None
-
-    def __init__(self, path, zero_point=None, pixel_rez=None):
-        """
-
-        """
-        super(ColormapTiles, self).__init__()
-        if path.lower().endswith('.json'):
-            from json import load
-            with open(path, 'rt') as fp:
-                info = load(fp)
-                globals().update(info)  # FIXME: security
-        self._image = im = Image.open(path)
-        # image is top-left to bottom-right, remember this when loading textures
-        im.load()
-        l,t,r,b = im.getbbox()
-        w = r-l
-        h = b-t
-        self._shape = (h,w)
-
-
-
-    def paint(self, geom, fast=False):
-        return True
-
-    def render(self, geom, *more_geom):
-        """
-        in offline GL, make a drawlist that nicely handles the world geometry requested
-        """
-
-        return True
-
-    def purge(self, geom, *more_geom):
-        return False
-
-    def probe_point_xy(self, x, y):
-        raise NotImplementedError()
-
-    def probe_point_geo(self, lat, lon):
-        raise NotImplementedError()
-
-    def probe_shape(self, geo_shape):
-        raise NotImplementedError()
-
-
-    # def tileseq_in_area(self, index_bltr):
-    #     """
-    #     yield the sequence of tiles in a given rectangular tileseq_in_area
-    #     """
-    #     pass
-    #
-    # def tileseq_visible(self, data_bltr):
-    #     """
-    #     given data coordinates, determine which tiles are on the canvas
-    #     """
-    #     pass
-    #
-
-
-class TestLayer(Layer):
-    def paint(self, *args):
-        glColor3f(0.0, 0.0, 1.0)
-        glRectf(-5, -5, 5, 5)
-        glColor3f(1.0, 0.0, 0.0)
-        glBegin(GL_LINES)
-        glVertex3f(0, 0, 0)
-        glVertex3f(20, 20, 0)
 
 
 
