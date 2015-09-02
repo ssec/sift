a079138f4c32669409823161ead4044a235ceb31
R. Keoni Garcia <keoni@tauriel.ssec.wisc.edu>
2015-09-02 11:16:18 -0500

    refactor GlooRGBTile to have base class GlooTile that we can reuse much of

--- py/cspov/view/Program.py
+++ py/cspov/view/Program.py
@@ -71,29 +71,26 @@ void main()
 }
 """
 
-
-class GlooRGBTile(object):
+class GlooTile(object):
     """
     A GL program which plots a planar tile with a texture map
     texture map is assumed oriented such that ascending pixel row is ascending coordinate from bottom left of screen
     (typically requires inverting the Y direction)
     """
     program = None
-    image = None
-    world_box = None
+    _world_box = None
     faces = None
     _z = 0.0
     _alpha = 1.0
 
-    def __init__(self, world_box=None, image=None, image_box=None):
-        super(GlooRGBTile, self).__init__()
-        self.program = gloo.Program(VERT_CODE, FRAG_CODE)
-        if image is not None:
-            self.set_texture(image, image_box)
-        if world_box is not None:
-            self.set_world(world_box)
+    def __init__(self, vertex_code, fragment_code):
+        super(GlooTile, self).__init__()
+        self.program = gloo.Program(vertex_code, fragment_code)
 
-    def set_world(self, world_box):
+    def get_world_box(self):
+        return self._world_box
+
+    def set_world_box(self, world_box):
         # get the geometry queued
         vtnc, faces, outline = create_plane(width=world_box.r-world_box.l,
                                              height=world_box.t-world_box.b,
@@ -114,24 +111,23 @@ class GlooRGBTile(object):
         # print("F:", faces, len(faces))
         # print("O:", outline, len(outline))
         # print("T:", texcoords, len(texcoords))
-
         self.program['a_position'] = gloo.VertexBuffer(verts)
         self.program['a_texcoord'] = gloo.VertexBuffer(texcoords)
         self.faces = faces_buffer
-        self.world_box = world_box
+        self._world_box = world_box
         self.program['u_z'] = self._z
         self.program['u_alpha'] = self._alpha
 
-    def set_texture(self, image, image_box=None):
-        if isinstance(image, str) and image == 'crate':
-            image = load_crate()
-        if image_box is not None:
-            self.image = image = image[image_box.b:image_box.t, image_box.l:image_box.r]
-            print("clipping")
-        else:
-            self.image = image
-        # get the texture queued
-        self.program['u_texture'] = gloo.Texture2D(image)
+    world_box = property(get_world_box, set_world_box)
+
+
+    def get_data(self):
+        raise NotImplementedError('subclass must implement')
+
+    def set_data(self, image, image_box=None):
+        raise NotImplementedError('subclass must implement')
+
+    data = property(get_data, set_data)
 
 
     def get_z(self):
@@ -163,6 +159,45 @@ class GlooRGBTile(object):
 
 
     def draw(self):
-        assert(self.image is not None)
-        assert(self.world_box is not None)
         self.program.draw('triangles', self.faces)
+
+
+
+class GlooRGBTile(GlooTile):
+    """
+    A GL program which plots a planar tile with a texture map
+    texture map is assumed oriented such that ascending pixel row is ascending coordinate from bottom left of screen
+    (typically requires inverting the Y direction)
+    """
+    program = None
+    image = None
+    _world_box = None
+    faces = None
+    _z = 0.0
+    _alpha = 1.0
+
+    def __init__(self, world_box=None, image=None, image_box=None):
+        super(GlooRGBTile, self).__init__(VERT_CODE, FRAG_CODE)
+        if image is not None:
+            self.set_data(image, image_box)
+        if world_box is not None:
+            self.set_world_box(world_box)
+
+
+    def get_data(self):
+        return self.image
+
+    def set_data(self, image, image_box=None):
+        if isinstance(image, str) and image == 'crate':
+            image = load_crate()
+        if image_box is not None:
+            self.image = image = image[image_box.b:image_box.t, image_box.l:image_box.r]
+            print("clipping")
+        else:
+            self.image = image
+        # get the texture queued
+        self.program['u_texture'] = gloo.Texture2D(image)
+
+
+
+
