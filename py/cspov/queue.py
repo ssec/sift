44e59776dc0ad3487793d66c8c3b139352b073b4
R. Keoni Garcia <keoni@Tauriel.local>
2015-10-30 15:32:14 -0500

    3 worker threads - two interactive, one background. currently everything going to background queue so it should be have like 0.5.1 master. Next to enable interactive=True on retiles

--- py/cspov/queue.py
+++ py/cspov/queue.py
@@ -36,49 +36,26 @@ TASK_PROGRESS = ("progress", float) # 0.0 - 1.0 progress
 # singleton instance used by clients
 TheQueue = None
 
-
-class TaskQueue(QThread):
+class Worker(QThread):
     """
-    Global background task queue for loading, rendering, et cetera.
-    Includes state updates and GUI links.
-    Eventually will include thread pools and multiprocess pools.
+    Worker thread use by TaskQueue
     """
-    process_pool = None  # process pool for background activity
-    thread_pool = None  # thread pool for background activity
     queue = None
     depth = 0
 
-    didMakeProgress = pyqtSignal(list)  # sequence of dictionaries listing update information to be propagated to view
-    # started : inherited
-    # finished : inherited
-    # terminated : inherited
+    workerDidMakeProgress = pyqtSignal(int, list)  # worker id, sequence of dictionaries listing update information to be propagated to view
 
-    def __init__(self, process_pool=None, thread_pool=None):
-        super(TaskQueue, self).__init__()
-        self.process_pool = process_pool
-        self.thread_pool = thread_pool
+    def __init__(self, myid:int):
+        super(Worker, self).__init__()
         self.queue = []
-        global TheQueue
-        assert(TheQueue is None)
-        TheQueue = self
+        self.depth = 0
+        self.id = myid
 
-    def add(self, key, task_iterable, description, use_process_pool=False, use_thread_pool=False):
-        """
-        Add an iterable task which will yield progress information dictionaries.
-
-        Expect behavior like this:
-         for task in queue:
-            for status_info in task:
-                update_display(status_info)
-            pop_display(final_status_info)
-
-        :param key: unique key for task; queuing the same key will result in the old task being removed and the new one deferred to the end
-        :param value: iterable task object
-        :return:
-        """
+    def add(self, key, task_iterable):
+        # FUTURE: replace queued task if key matches
         self.queue.append(task_iterable)
         self.depth += 1
-        self.start()  # safe if already running
+        self.start()
 
     def _did_progress(self, task_status):
         """
@@ -87,17 +64,8 @@ class TaskQueue(QThread):
         :return:
         """
         # FIXME: this should have entries for the upcoming stuff as well so we can have an Activity panel
-        info = [task_status]
-        self.didMakeProgress.emit(info)
-
-    def progress_ratio(self, current_progress=None):
-        if self.depth == 0:
-            return 0.0
-        elif self.depth == 1 and current_progress is not None:
-            # show something other than 50% if there is only 1 job
-            return current_progress
-        else:
-            return float(self.depth - len(self.queue)) / self.depth
+        info = [task_status] if task_status else []
+        self.workerDidMakeProgress.emit(self.id, info)
 
     def run(self):
         while len(self.queue)>0:
@@ -109,7 +77,103 @@ class TaskQueue(QThread):
                 LOG.error("Background task failed")
                 LOG.debug("Background task exception: ", exc_info=True)
         self.depth = 0
-        self._did_progress({TASK_DOING:'', TASK_PROGRESS:0.0})
+        self._did_progress(None)
+
+
+class TaskQueue(QObject):
+    """
+    Global background task queue for loading, rendering, et cetera.
+    Includes state updates and GUI links.
+    Eventually will include thread pools and multiprocess pools.
+    Two threads for interactive tasks (high priority), one thread for background tasks (low priority): 0, 1, 2
+    """
+    process_pool = None  # process pool for background activity
+    workers = None  # thread pool for background activity
+    _interactive_round_robin = 0  # swaps between 0 and 1 for interactive tasks
+    _last_status = None  # list of last status reports for different workers
+
+    didMakeProgress = pyqtSignal(list)  # sequence of dictionaries listing update information to be propagated to view
+    # started : inherited
+    # finished : inherited
+    # terminated : inherited
+
+    def __init__(self, process_pool=None, worker_count=3):
+        super(TaskQueue, self).__init__()
+        self._interactive_round_robin = 0
+        self.process_pool = process_pool
+        self._last_status = []
+        self.workers = []
+        for id in range(3):
+            worker = Worker(id)
+            worker.workerDidMakeProgress.connect(self._did_progress)
+            self.workers.append(worker)
+            self._last_status.append(None)
+
+        global TheQueue
+        assert(TheQueue is None)
+        TheQueue = self
+
+    @property
+    def depth(self):
+        return sum([x.depth for x in self.workers])
+
+    @property
+    def remaining(self):
+        return sum([len(x.queue) for x in self.workers])
+
+    def add(self, key, task_iterable, description, interactive=False, use_process_pool=False, use_thread_pool=False):
+        """
+        Add an iterable task which will yield progress information dictionaries.
+
+        Expect behavior like this:
+         for task in queue:
+            for status_info in task:
+                update_display(status_info)
+            pop_display(final_status_info)
+
+        :param key: unique key for task; queuing the same key will result in the old task being removed and the new one deferred to the end
+        :param task_iterable: callable resulting in an iterable, or an iterable itself to be run on the background
+        :return:
+        """
+        if interactive:
+            wdex = self._interactive_round_robin
+            self._interactive_round_robin += 1
+            self._interactive_round_robin %= 2
+        else:
+            wdex = 2
+        self.workers[wdex].add(key, task_iterable)
+
+    def _did_progress(self, worker_id, worker_status):
+        """
+        Summarize the task queue, including progress, and send it out as a signal
+        :param worker_status: list of active items, or at least the thing it's working on now
+        :return:
+        """
+        # FIXME: this should consolidate entries for the upcoming stuff as well so we can have an Activity panel
+
+        self._last_status[worker_id] = worker_status
+
+        # report on the lowest worker number that's active; (0,1 interactive; 2 background)
+        # yes, this will be redundant and #FUTURE make this a more useful signal content, rather than relying on progress_ratio back-query
+        for wdex,status in enumerate(self._last_status):
+            if self.workers[wdex].isRunning():
+                self.didMakeProgress.emit(status)
+                return
+
+        # otherwise this is a notification that we're finally at full idle
+        self.didMakeProgress.emit([{TASK_DOING: '', TASK_PROGRESS: 0.0}])
+        # FUTURE: consider one progress bar per worker
+
+    def progress_ratio(self, current_progress=None):
+        depth = self.depth
+        if depth == 0:
+            return 0.0
+        elif depth == 1 and current_progress is not None:
+            # show something other than 50% if there is only 1 job
+            return current_progress
+        else:
+            depth, remaining = self.depth, self.remaining
+            return float(depth - remaining) / depth
 
 
 def test_task():
