66794f812ea6b258ae464441a317f999495f9204
R. Keoni Garcia <keoni@Tauriel.local>
2015-11-02 18:49:38 -0600

    v0.7.0 prep

--- py/cspov/ui/pov_main_ui.py
+++ py/cspov/ui/pov_main_ui.py
@@ -267,7 +267,7 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "SIFT Beta 0.6.0", None))
+        MainWindow.setWindowTitle(_translate("MainWindow", "SIFT Beta 0.7.0", None))
         self.panZoomToolButton.setToolTip(_translate("MainWindow", "Pan/Zoom Tool", None))
         self.panZoomToolButton.setStatusTip(_translate("MainWindow", "Left mouse click and drag pans the scene", None))
         self.panZoomToolButton.setText(_translate("MainWindow", "Pan/Zoom", None))
