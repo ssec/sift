d7ade8077565207d83b3680ff71891411d1cfd2b
davidh-ssec <david.hoese@ssec.wisc.edu>
2016-08-18 15:18:35 -0500

    Update bump version command on setup.py

--- py/setup.py
+++ py/setup.py
@@ -14,6 +14,19 @@ command.
     PyQt4 is required for GUI operations, but must be install manually
     since it is not 'pip' installable.
 
+For Developers
+--------------
+
+To bump the version run:
+
+    python setup.py bump -b minor -d alpha
+
+Or to tag and commit the new version:
+
+    python setup.py bump -b major -t -c
+
+See the `-h` options for more info.
+
 """
 
 import os
@@ -23,7 +36,7 @@ from setuptools import setup, find_packages, Command
 script_dir = os.path.dirname(os.path.realpath(__file__))
 version_pathname = os.path.join(script_dir, "cspov", "version.py")
 version_str = open(version_pathname).readlines()[-1].split()[-1].strip("\"\'")
-version_regex = re.compile('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)(?P<dev_level>[ab]?)(?P<dev_version>\d+)?')
+version_regex = re.compile('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)(?:(?P<dev_level>[ab])(?P<dev_version>\d))?$')
 version_info = version_regex.match(version_str).groupdict()
 assert version_info is not None, "Invalid version in version.py: {}".format(version_str)
 version_info["major"] = int(version_info["major"])
@@ -41,14 +54,20 @@ class BumpCommand(Command):
     description = "bump package version by one micro, minor, or major version number (major.minor.micro[a/b])"
     user_options = [
         ("bump-level=", 'b', "major, minor, micro (default: None)"),
-        ("dev-level=", 'd', "alpha or beta (default: None"),
+        ("dev-level=", 'd', "alpha or beta (default: None)"),
+        ("new-version=", 'v', "specify exact new version number (default: None"),
+        ("dry-run", 'n', "dry run, don't change anything"),
+        ("tag", "t", "add a git tag for this version (default: False)"),
         ("commit", "c", "Run the git commit command but do not push (default: False)"),
     ]
-    boolean_options = ["commit"]
+    boolean_options = ["dry_run", "tag", "commit"]
 
     def initialize_options(self):
         self.bump_level = None
         self.dev_level = None
+        self.new_version = None
+        self.dry_run = False
+        self.tag = False
         self.commit = False
 
     def finalize_options(self):
@@ -60,33 +79,41 @@ class BumpCommand(Command):
     def run(self):
         current_version = version_info
         new_version = current_version.copy()
-        if self.bump_level == "micro":
-            new_version["micro"] += 1
-        elif self.bump_level == "minor":
-            new_version["minor"] += 1
-            new_version["micro"] = 0
-        elif self.bump_level == "major":
-            new_version["major"] += 1
-            new_version["minor"] = 0
-            new_version["micro"] = 0
-        new_version_str = "{major:d}.{minor:d}.{micro:d}".format(**new_version)
+        if self.new_version is not None:
+            new_version_str = self.new_version
+            assert(version_regex.match(new_version_str) is not None)
+        else:
+            if self.bump_level == "micro":
+                new_version["micro"] += 1
+            elif self.bump_level == "minor":
+                new_version["minor"] += 1
+                new_version["micro"] = 0
+            elif self.bump_level == "major":
+                new_version["major"] += 1
+                new_version["minor"] = 0
+                new_version["micro"] = 0
+            new_version_str = "{major:d}.{minor:d}.{micro:d}".format(**new_version)
 
-        if self.dev_level == "alpha":
-            if current_version["dev_level"] == "a" and self.bump_level is None:
-                new_dev_version = current_version["dev_version"] + 1
-            else:
-                new_dev_version = 0
-            new_version_str += "a{:d}".format(new_dev_version)
-        elif self.dev_level == "beta":
-            if current_version["dev_level"] == "b" and self.bump_level is None:
-                new_dev_version = current_version["dev_version"] + 1
-            else:
-                new_dev_version = 0
-            new_version_str += "b{:d}".format(new_dev_version)
+            if self.dev_level == "alpha":
+                if current_version["dev_level"] == "a" and self.bump_level is None:
+                    new_dev_version = current_version["dev_version"] + 1
+                else:
+                    new_dev_version = 0
+                new_version_str += "a{:d}".format(new_dev_version)
+            elif self.dev_level == "beta":
+                if current_version["dev_level"] == "b" and self.bump_level is None:
+                    new_dev_version = current_version["dev_version"] + 1
+                else:
+                    new_dev_version = 0
+                new_version_str += "b{:d}".format(new_dev_version)
 
         # Update the version test in the version.py file
         print("Old Version: {}".format(version_str))
         print("New Version: {}".format(new_version_str))
+
+        if self.dry_run:
+            print("### Dry Run: No modifications ###")
+            return
 
         # Update the version.py
         print("Updating version.py...")
@@ -107,25 +134,29 @@ class BumpCommand(Command):
         # Tag git repository commit
         add_args = ["git", "add", version_pathname, iss_pathname]
         commit_args = ["git", "commit", "-m", "Bump version from {} to {}".format(version_str, new_version_str)]
-        tag_args = ["git", "tag", "-a", new_version_str, "-m", "Version {}".format(new_version_str)]
         if self.commit:
             import subprocess
             print("Adding files to git staging area...")
             subprocess.check_call(add_args)
             print("Committing changes...")
             subprocess.check_call(commit_args)
-            print("Tagging commit...")
-            subprocess.check_call(tag_args)
         else:
             commit_args[-1] = "\"" + commit_args[-1] + "\""
-            tag_args[-1] = "\"" + tag_args[-1] + "\""
             print("To appropriate files:")
             print("    ", " ".join(add_args))
             print("To commit after run:")
             print("    ", " ".join(commit_args))
-            print("Followed by:")
+
+        tag_args = ["git", "tag", "-a", new_version_str, "-m", "Version {}".format(new_version_str)]
+        if self.tag:
+            print("Tagging commit...")
+            subprocess.check_call(tag_args)
+        else:
+            tag_args[-1] = "\"" + tag_args[-1] + "\""
+            print("To tag:")
             print("    ", " ".join(tag_args))
-        print("Run:\n    git push --follow-tags")
+
+        print("To push git changes to remote, run:\n    git push --follow-tags")
 
 setup(
     name='cspov',
