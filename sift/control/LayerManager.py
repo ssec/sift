Fix unit conversion to be a little more flexible

sift/control/LayerManager.py
@@ -303,11 +303,29 @@ class SingleLayerInfoPane (QWidget) :
                 new_clims = ""
                 if this_prez is not None:
                     new_clims = np.array(this_prez.climits)
-                    fmt, unit, new_clims = self.document.convert_units(this_prez.uuid, new_clims, inverse=False)
+                    unit_info = self.document[this_prez.uuid][INFO.UNIT_CONVERSION]
+                    new_clims = unit_info[1](new_clims, inverse=False)
                     try:
-                        new_clims = (fmt + ' ~ ' + fmt + '{}').format(new_clims[0], new_clims[1], unit)
+                        if layer_info[INFO.KIND] == KIND.IMAGE:
+                            min_str = layer_info[INFO.UNIT_CONVERSION][2](new_clims[0], include_units=False)
+                            max_str = layer_info[INFO.UNIT_CONVERSION][2](new_clims[1])
+                            new_clims = '{} ~ {}'.format(min_str, max_str)
+                        else:
+                            # FUTURE: Other layer types
+                            deps = (layer_info.r, layer_info.g, layer_info.b)
+
+                            tmp_clims = []
+                            for i, dep in enumerate(deps):
+                                if dep is None:
+                                    tmp_clims.append('N/A')
+                                    continue
+
+                                min_str = dep[INFO.UNIT_CONVERSION][2](new_clims[i][0], include_units=False)
+                                max_str = dep[INFO.UNIT_CONVERSION][2](new_clims[i][1])
+                                tmp_clims.append('{} ~ {}'.format(min_str, max_str))
+                            new_clims = ", ".join(tmp_clims)
                     except TypeError as err:
-                        LOG.warning("unknown color limit format for %s" % repr((fmt, unit, new_clims)))
+                        LOG.warning("unable to format color limit: %r" % (new_clims,), exc_info=True)
                         new_clims = "N/A"
                 if "climits" not in shared_info :
                     shared_info["climits"] = new_clims
@@ -490,16 +508,14 @@ class RGBLayerConfigPane(QWidget):
         uuid = self._uuids[RGBA2IDX[color]]
         if not uuid:
             return values
-        format, units, data= self.document_ref().convert_units(uuid, values, inverse=True)
-        return data
+        return self.document_ref()[uuid][INFO.UNIT_CONVERSION][1](values, inverse=True)
 
     def _data_to_display(self, color:str, values):
         "convert data value to display value"
         uuid = self._uuids[RGBA2IDX[color]]
         if not uuid:
             return values
-        format, units, display = self.document_ref().convert_units(uuid, values, inverse=False)
-        return display
+        return self.document_ref()[uuid][INFO.UNIT_CONVERSION][1](values)
 
     def _get_slider_value(self, valid_min, valid_max, slider_val):
         return (slider_val / self._slider_steps) * (valid_max - valid_min) + valid_min

sift/model/document.py
@@ -305,6 +305,7 @@ class Document(QObject):  # base class is rightmost, mixins left of that
         layer[INFO.CLIM] = self._guidebook.climits(layer)
         layer[INFO.DISPLAY_TIME] = self._guidebook._default_display_time(layer)
         layer[INFO.DISPLAY_NAME] = self._guidebook._default_display_name(layer)
+        layer[INFO.UNIT_CONVERSION] = self._guidebook.units_conversion(layer)
 
         return layer
 
@@ -412,15 +413,11 @@ class Document(QObject):  # base class is rightmost, mixins left of that
         # in the future valid range may be different than the default CLIMs
         return self[uuid][INFO.CLIM]
 
-    def convert_units(self, uuid, data, inverse=False):
-        """
-        :param uuid: layer id
-        :param data: values to convert
-        :param inverse: when true, convert from display units to data units
-        :return:
-        """
-        formatstr, unitstr, lam = self._guidebook.units_conversion(self._layer_with_uuid[uuid])
-        return formatstr, unitstr, lam(data, inverse)
+    def convert_value(self, uuid, x, inverse=False):
+        return self[uuid][INFO.UNIT_CONVERSION][1](x, inverse=inverse)
+
+    def format_value(self, uuid, x, numeric=True, units=True):
+        return self[uuid][INFO.UNIT_CONVERSION][2](x, numeric=numeric, units=units)
 
     def flipped_for_uuids(self, uuids, lset=None):
         for p in self.prez_for_uuids(uuids, lset=lset):
@@ -444,28 +441,25 @@ class Document(QObject):  # base class is rightmost, mixins left of that
             lyr = self._layer_with_uuid[pinf.uuid]
             if lyr[INFO.KIND] == KIND.IMAGE:
                 value = self._workspace.get_content_point(pinf.uuid, xy_pos)
-                fmt, unit_str, unit_conv = self._guidebook.units_conversion(self._layer_with_uuid[pinf.uuid])
+                unit_info = self._guidebook.units_conversion(self._layer_with_uuid[pinf.uuid])
+                nc, xc = unit_info[1](np.array(pinf.climits))
                 # calculate normalized bar width relative to its current clim
-                new_value = unit_conv(value)
-                if isinstance(new_value, str):
-                    # assume that the values weren't converted do different units (use pre-unit_conv)
-                    nc, xc = pinf.climits
-                    bar_width = (np.clip(value, nc, xc) - nc) / (xc - nc)
-                    zult[pinf.uuid] = (new_value, bar_width, fmt, unit_str)
+                new_value = unit_info[1](value)
+                if nc > xc:  # sometimes clim is swapped to reverse color scale
+                    nc, xc = xc, nc
+                if np.isnan(new_value):
+                    zult[pinf.uuid] = None
                 else:
-                    nc, xc = unit_conv(np.array(pinf.climits))
-                    if nc > xc:  # sometimes clim is swapped to reverse color scale
-                        nc, xc = xc, nc
-                    if np.isnan(new_value):
-                        zult[pinf.uuid] = None
-                    else:
-                        bar_width = (np.clip(new_value, nc, xc) - nc) / (xc - nc)
-                        zult[pinf.uuid] = (new_value, bar_width, fmt, unit_str)
+                    bar_width = (np.clip(new_value, nc, xc) - nc) / (xc - nc)
+                    zult[pinf.uuid] = (new_value, bar_width, unit_info[2](new_value, numeric=False))
             elif lyr[INFO.KIND] == KIND.RGB:
                 # We can show a valid RGB
                 # Get 3 values for each channel
                 # XXX: Better place for this?
                 def _sci_to_rgb(v, cmin, cmax):
+                    if np.isnan(v):
+                        return None
+
                     if cmin == cmax:
                         return 0
                     elif cmin > cmax:
@@ -490,15 +484,12 @@ class Document(QObject):  # base class is rightmost, mixins left of that
                         value = self._workspace.get_content_point(dep_lyr[INFO.UUID], xy_pos)
                         values.append(_sci_to_rgb(value, clims[0], clims[1]))
 
-                # fmt = "{:3d}"
-                fmt = "{}"
-                unit_str = ""
                 nc = 0
                 xc = 255
                 bar_widths = [(np.clip(value, nc, xc) - nc) / (xc - nc) for value in values if value is not None]
                 bar_width = np.mean(bar_widths) if len(bar_widths) > 0 else 0
                 values = ",".join(["{:3d}".format(v if v is not None else 0) for v in values])
-                zult[pinf.uuid] = (values, bar_width, fmt, unit_str)
+                zult[pinf.uuid] = (values, bar_width, values)
 
         self.didCalculateLayerEqualizerValues.emit(zult)  # is picked up by layer list model to update display
 
@@ -817,6 +808,9 @@ class Document(QObject):  # base class is rightmost, mixins left of that
             INFO.UUID: uuid,
             INFO.KIND: KIND.RGB,
         }
+
+        self._layer_with_uuid[uuid] = ds_info = DocRGBLayer(self, ds_info)
+
         changeable_meta = {
             INFO.DATASET_NAME: '-RGB-',
             INFO.DISPLAY_NAME: '-RGB-',
@@ -826,11 +820,11 @@ class Document(QObject):  # base class is rightmost, mixins left of that
             INFO.ORIGIN_Y: None,
             INFO.CELL_WIDTH: None,
             INFO.CELL_HEIGHT: None,
-            INFO.CLIM: (None, None, None),
+            INFO.CLIM: ((None, None), (None, None), (None, None)),
+            INFO.UNIT_CONVERSION: self._guidebook.units_conversion(ds_info),
         }
-
-        self._layer_with_uuid[uuid] = ds_info = DocRGBLayer(self, ds_info)
         ds_info.update(changeable_meta)
+        ds_info.update_metadata_from_dependencies()
         presentation, reordered_indices = self._insert_layer_with_info(ds_info)
 
         LOG.info('generating incomplete (invalid) composite for user to configure')
@@ -839,7 +833,7 @@ class Document(QObject):  # base class is rightmost, mixins left of that
         color_assignments = {}
         def _(color, lyr):
             if lyr:
-                color_assignments[color] = self._layer_with_uuid[lyr] if isinstance(lyr, UUID) else lyr
+                color_assignments[color] = self[lyr] if isinstance(lyr, UUID) else lyr
         _('r', r)
         _('g', g)
         _('b', b)

sift/model/guidebook.py
@@ -167,6 +167,14 @@ class AHI_HSF_Guidebook(Guidebook):
             except KeyError:
                 z[INFO.STANDARD_NAME] = z.get(INFO.SHORT_NAME)
 
+        # Only needed for backwards compatibility with originally supported geotiffs
+        if INFO.UNITS not in info:
+            standard_name = info.get(INFO.STANDARD_NAME, z.get(INFO.STANDARD_NAME))
+            if standard_name == 'toa_bidirectional_reflectance':
+                z[INFO.UNITS] = '1'
+            elif standard_name == 'toa_brightness_temperature':
+                z[INFO.UNITS] = 'kelvin'
+
         return z
 
     def _is_refl(self, dsi):
@@ -194,50 +202,101 @@ class AHI_HSF_Guidebook(Guidebook):
             # some kind of default
             return 0., 255.
 
+    def preferred_units(self, dsi):
+        # FUTURE: Use cfunits or cf_units package
+        if self._is_refl(dsi):
+            return '1'
+        elif self._is_bt(dsi):
+            return 'degrees_Celsius'
+        else:
+            return dsi.get(INFO.UNITS, None)
+
+    def unit_symbol(self, unit):
+        # FUTURE: Use cfunits or cf_units package
+        # cf_units gives the wrong symbol for celsius
+        if unit == '1':
+            return ''
+        elif unit == 'degrees_Celsius' or unit == 'C':
+            return '°C'
+        elif unit == 'kelvin' or unit == 'K':
+            return 'K'
+        else:
+            return unit or ""
+
+    def _unit_format_func(self, layer, units):
+        units = self.unit_symbol(units)
+
+        # default formatting string
+        def _format_unit(val, numeric=True, include_units=True):
+            return '{:.03f}{units:s}'.format(val, units=units if include_units else "")
+
+        if self._is_bt(layer):
+            # BT data limits, Kelvin to degC
+            def _format_unit(val, numeric=True, include_units=True):
+                return '{:.02f}{units}'.format(val, units=units if include_units else "")
+        elif "flag_values" in layer:
+            # flag values don't have units
+            if "flag_meanings" in layer:
+                flag_masks = layer["flag_masks"] if "flag_masks" in layer else [-1] * len(layer["flag_values"])
+                flag_info = tuple(zip(layer["flag_meanings"], layer["flag_values"], flag_masks))
+                def _format_unit(val, numeric=True, include_units=True, flag_info=flag_info):
+                    val = int(val)
+                    if numeric:
+                        return '{:d}'.format(val)
+
+                    meanings = []
+                    for fmean, fval, fmask in flag_info:
+                        if (val & fmask) == fval:
+                            meanings.append(fmean)
+                    return "{:d} ({:s})".format(val, ", ".join(meanings))
+            else:
+                def _format_unit(val, numeric=True, include_units=True):
+                    return '{:d}'.format(int(val))
+
+        return _format_unit
+
+    def units_conversion_rgb(self, dsi):
+        def conv_func(x, inverse=False, deps=(dsi.r, dsi.g, dsi.b)):
+            if isinstance(x, np.ndarray):
+                # some sort of array
+                x_tmp = x.ravel()
+                assert x_tmp.size % len(deps) == 0
+                num_elems = x_tmp.size // len(deps)
+                new_vals = []
+                for i, dep in enumerate(deps):
+                    new_val = x_tmp[i * num_elems: (i + 1) * num_elems]
+                    if dep is not None:
+                        new_val = dep[INFO.UNIT_CONVERSION][1](new_val, inverse=inverse)
+                    new_vals.append(new_val)
+                res = np.array(new_vals).reshape(x.shape)
+                return res
+            else:
+                # Not sure this should ever happen (should always be at least 3
+                return x
+
+        def format_func(val, numeric=True, include_units=False):
+            return ", ".join("{}".format(v) if v is None else "{:0.03f}".format(v) for v in val)
+        return None, conv_func, format_func
+
     def units_conversion(self, dsi):
         "return UTF8 unit string, lambda v,inverse=False: convert-raw-data-to-unis"
-        units = dsi.get("units", "")
-        if units == "1":
-            units = ""
+        if dsi[INFO.KIND] == KIND.RGB:
+            return self.units_conversion_rgb(dsi)
 
-        if self._is_refl(dsi):
-            # Reflectance/visible data limits
-            return ('{:.03f}', '', lambda x, inverse=False: x)
-        elif self._is_bt(dsi):
-            # BT data limits, Kelvin to degC
-            return ("{:.02f}", "°C", lambda x, inverse=False: x - 273.15 if not inverse else x + 273.15)
-        elif "flag_values" in dsi:
-            # XXX: Better place to put all this logic?
-            if "flag_meanings" in dsi:
-                flag_masks = dsi["flag_masks"] if "flag_masks" in dsi else [-1] * len(dsi["flag_values"])
-                def _flag_meaning(x, inverse=False):
-                    single = False
-                    if isinstance(x, np.ndarray):
-                        if x.size == 2:
-                            # assume color limits
-                            return [str(val) for val in x]
-                    else:
-                        x = [x]
-                        single = True
+        # the dataset might be in one unit, but the user may want something else
+        # FUTURE: Use cfunits or cf_units package
+        preferred_units = self.preferred_units(dsi)
 
-                    all_strings = []
-                    for data_val in x:
-                        meanings = []
-                        data_val = int(data_val)
-                        for fmean, fval, fmask in zip(dsi["flag_meanings"], dsi["flag_values"], flag_masks):
-                            if (data_val & fmask) == fval:
-                                meanings.append(fmean)
-                        s = "{:d} ({:s})".format(data_val, ", ".join(meanings))
-                        all_strings.append(s)
-                    if single:
-                        return all_strings[0]
-                    return all_strings
-
-                return ("{:s}", units, _flag_meaning)
-            else:
-                return ("{:0.0f}", units, lambda x, inverse=False: x)
+        # Conversion functions
+        # FUTURE: Use cfunits or cf_units package
+        if dsi.get(INFO.UNITS) in ('kelvin', 'K') and preferred_units in ('degrees_Celsius', 'C'):
+            conv_func = lambda x, inverse=False: x - 273.15 if not inverse else x + 273.15
         else:
-            return ("{:.03f}", units, lambda x, inverse=False: x)
+            conv_func = lambda x, inverse=False: x
+
+        # Format strings
+        format_func = self._unit_format_func(dsi, preferred_units)
+        return (preferred_units, conv_func, format_func)
 
     def default_colormap(self, dsi):
         return DEFAULT_COLORMAPS.get(dsi.get(INFO.STANDARD_NAME), DEFAULT_UNKNOWN)