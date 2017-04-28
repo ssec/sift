7588d6d33d010e70c8ca498fc3e7cbe53da40e75
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-04-28 14:12:41 -0500

    Add export image dialog qt files
    Forgot to add them

---
+++ sift/ui/export_image_dialog_ui.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'export_image_dialog.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_ExportImageDialog(object):
+    def setupUi(self, ExportImageDialog):
+        ExportImageDialog.setObjectName(_fromUtf8("ExportImageDialog"))
+        ExportImageDialog.resize(340, 226)
+        ExportImageDialog.setStyleSheet(_fromUtf8(""))
+        self.buttonBox = QtGui.QDialogButtonBox(ExportImageDialog)
+        self.buttonBox.setEnabled(True)
+        self.buttonBox.setGeometry(QtCore.QRect(20, 190, 301, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Save)
+        self.buttonBox.setCenterButtons(False)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.frameRangeGroupBox = QtGui.QGroupBox(ExportImageDialog)
+        self.frameRangeGroupBox.setGeometry(QtCore.QRect(10, 40, 321, 111))
+        self.frameRangeGroupBox.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.frameRangeGroupBox.setFlat(False)
+        self.frameRangeGroupBox.setCheckable(False)
+        self.frameRangeGroupBox.setObjectName(_fromUtf8("frameRangeGroupBox"))
+        self.frameCurrentRadio = QtGui.QRadioButton(self.frameRangeGroupBox)
+        self.frameCurrentRadio.setGeometry(QtCore.QRect(10, 20, 121, 20))
+        self.frameCurrentRadio.setChecked(True)
+        self.frameCurrentRadio.setObjectName(_fromUtf8("frameCurrentRadio"))
+        self.frameAllRadio = QtGui.QRadioButton(self.frameRangeGroupBox)
+        self.frameAllRadio.setGeometry(QtCore.QRect(10, 50, 100, 20))
+        self.frameAllRadio.setObjectName(_fromUtf8("frameAllRadio"))
+        self.frameRangeRadio = QtGui.QRadioButton(self.frameRangeGroupBox)
+        self.frameRangeRadio.setGeometry(QtCore.QRect(10, 80, 100, 20))
+        self.frameRangeRadio.setObjectName(_fromUtf8("frameRangeRadio"))
+        self.label = QtGui.QLabel(self.frameRangeGroupBox)
+        self.label.setGeometry(QtCore.QRect(110, 80, 41, 20))
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.frameRangeFrom = QtGui.QLineEdit(self.frameRangeGroupBox)
+        self.frameRangeFrom.setGeometry(QtCore.QRect(150, 80, 61, 20))
+        self.frameRangeFrom.setText(_fromUtf8(""))
+        self.frameRangeFrom.setReadOnly(False)
+        self.frameRangeFrom.setPlaceholderText(_fromUtf8(""))
+        self.frameRangeFrom.setProperty("clearButtonEnabled", False)
+        self.frameRangeFrom.setObjectName(_fromUtf8("frameRangeFrom"))
+        self.label_2 = QtGui.QLabel(self.frameRangeGroupBox)
+        self.label_2.setGeometry(QtCore.QRect(230, 80, 21, 20))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.frameRangeTo = QtGui.QLineEdit(self.frameRangeGroupBox)
+        self.frameRangeTo.setGeometry(QtCore.QRect(250, 80, 61, 20))
+        self.frameRangeTo.setObjectName(_fromUtf8("frameRangeTo"))
+        self.includeFooterCheckbox = QtGui.QCheckBox(ExportImageDialog)
+        self.includeFooterCheckbox.setGeometry(QtCore.QRect(20, 160, 121, 20))
+        self.includeFooterCheckbox.setChecked(True)
+        self.includeFooterCheckbox.setObjectName(_fromUtf8("includeFooterCheckbox"))
+        self.saveAsLineEdit = QtGui.QLineEdit(ExportImageDialog)
+        self.saveAsLineEdit.setGeometry(QtCore.QRect(10, 10, 291, 21))
+        self.saveAsLineEdit.setText(_fromUtf8(""))
+        self.saveAsLineEdit.setObjectName(_fromUtf8("saveAsLineEdit"))
+        self.saveAsButton = QtGui.QPushButton(ExportImageDialog)
+        self.saveAsButton.setGeometry(QtCore.QRect(300, 10, 31, 21))
+        self.saveAsButton.setObjectName(_fromUtf8("saveAsButton"))
+
+        self.retranslateUi(ExportImageDialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), ExportImageDialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), ExportImageDialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(ExportImageDialog)
+
+    def retranslateUi(self, ExportImageDialog):
+        ExportImageDialog.setWindowTitle(_translate("ExportImageDialog", "Export Image", None))
+        self.frameRangeGroupBox.setTitle(_translate("ExportImageDialog", "Frame Range", None))
+        self.frameCurrentRadio.setText(_translate("ExportImageDialog", "Current", None))
+        self.frameAllRadio.setText(_translate("ExportImageDialog", "All", None))
+        self.frameRangeRadio.setText(_translate("ExportImageDialog", "Frames", None))
+        self.label.setText(_translate("ExportImageDialog", "from:", None))
+        self.label_2.setText(_translate("ExportImageDialog", "to:", None))
+        self.includeFooterCheckbox.setText(_translate("ExportImageDialog", "Include Footer", None))
+        self.saveAsButton.setText(_translate("ExportImageDialog", "...", None))
+
