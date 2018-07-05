af618ed2f2d8b71bc0b50d56bcec116939f8a473
davidh-ssec <david.hoese@ssec.wisc.edu>
2018-07-05 07:29:47 -0500

    Add pygrib to dependencies

--- setup.py
+++ setup.py
@@ -188,6 +188,7 @@ setup(
                       'PyOpenGL', 'netCDF4', 'h5py', 'pyproj', 'gdal',
                       'pyshp', 'shapely', 'rasterio', 'goesr', 'sqlalchemy',
                       'goesr', 'appdirs', 'pyyaml', 'pyqtgraph', 'satpy',
+                      'pygrib'
                       ],
     python_requires='>=3.6',
     extras_require=extras_require,
