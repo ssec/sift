7963fb9f29c0d6aafef0887f1ac1c1b80b843969
David Hoese <david.hoese@ssec.wisc.edu>
2019-10-11 15:51:45 -0500

    Add initial proof of concept global config object

--- uwsift/__init__.py
+++ uwsift/__init__.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python
 
+import os
+from donfig import Config
 from .version import __version__  # noqa
+from .util.default_paths import DOCUMENT_SETTINGS_DIR
+
+CONFIG_DIR = os.path.join(DOCUMENT_SETTINGS_DIR, 'config')
+os.environ.setdefault('UWSIFT_CONFIG', CONFIG_DIR)
+CONFIG_PATHS = [
+    CONFIG_DIR,
+    os.path.join(os.path.expanduser('~'), '.config', 'uwsift'),
+]
+
+DEFAULT_CONFIGURATION = {
+    'open_file_wizard': {
+        'readers': None,  # all readers
+        'id_components': [
+            'name',
+            'wavelength',
+            'calibration',
+            'level',
+        ],
+        'exclude_datasets': {
+            'calibration': ['radiance', 'counts'],
+        }
+    }
+}
+
+config = Config('uwsift', defaults=[DEFAULT_CONFIGURATION], paths=CONFIG_PATHS)
