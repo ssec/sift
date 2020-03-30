8a431d091e8be1573c0f2b1f9bd62159103a897c
Nicolai Kellerer <nicolai.kellerer@askvisual.de>
2020-03-30 15:06:25 +0200

    Enable to import Satpy from configured location
    by prepending a custom PathFinder into the system package search path
    and instruct it to look for satpy at the location given by the config
    setting 'satpy_import_path'.
    
    Also add an according config file which - if used - would set that
    setting to 'None', which is interpreted as to don't change the default
    import behaviour for Satpy.

--- uwsift/__init__.py
+++ uwsift/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import os
+import sys
+from importlib.machinery import PathFinder
+from importlib.util import spec_from_file_location
+
 from donfig import Config
 from .version import __version__  # noqa
 from .util.default_paths import DOCUMENT_SETTINGS_DIR
@@ -74,6 +78,45 @@ DEFAULT_CONFIGURATION = {
             'level',
         ],
     },
+
+    'satpy_import_path': None,
 }
 
 config = Config('uwsift', defaults=[DEFAULT_CONFIGURATION], paths=CONFIG_PATHS)
+
+
+def overwrite_import(package_name: str, custom_import_path: str, *, verbose=True):
+    if (custom_import_path is not None) and (not os.path.exists(custom_import_path)):
+        raise FileNotFoundError(f"custom package `{package_name}` doesn't exist: {custom_import_path}")
+
+    class CustomPathFinder(PathFinder):
+        @classmethod
+        def find_spec(cls, fullname: str, path=None, target=None):
+            if fullname.startswith(package_name):
+                if custom_import_path is not None:
+                    package_parts = fullname.split(".")
+                    assert package_parts[0] == package_name
+                    del package_parts[0]
+
+                    if len(package_parts) == 0:
+                        # import the root of the package
+                        package_import_path = os.path.join(custom_import_path, "__init__.py")
+                        spec = spec_from_file_location(fullname, package_import_path)
+                    else:
+                        # import the base directory of the subpackage
+                        package_parts.pop()
+                        package_import_path = os.path.join(custom_import_path, *package_parts)
+                        spec = super().find_spec(fullname, [package_import_path], target)
+
+                    if verbose:
+                        # setup for the logger happens in __main__, thus use print
+                        print(f"Custom import path for package `{fullname}`: {package_import_path}")
+                    return spec
+            return None
+
+    # The CustomPathFinder must have high priority, therefore set it to the front of the list.
+    # This is harmless, because we modify only the import path of the Satpy module.
+    sys.meta_path.insert(0, CustomPathFinder)
+
+
+overwrite_import("satpy", config["satpy_import_path"])
