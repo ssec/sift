1357417404be3ca4f05b9c0fa4704f429491a2a4
unknown <kkolman@wisc.edu>
2020-05-28 23:34:21 -0500

    Update .bat script

--- bundle_scripts/SIFT.bat
+++ bundle_scripts/SIFT.bat
@@ -8,11 +8,19 @@ call %base_dir%Scripts\activate
 
 REM Create a signal file that we have run conda-unpack
 set installed=%base_dir%.installed
-if not exist "%installed%" (
+if not exist "%installed%" goto install
+
+set /p install_dir=< %installed%
+if not %base_dir% == %install_dir:~0,-1% goto install
+
+goto run_sift
+
+:install 
   echo Running one-time initialization of SIFT installation...
   conda-unpack
   echo %base_dir% > %installed%
-)
+
+:run_sift
 
 echo Running SIFT...
 
