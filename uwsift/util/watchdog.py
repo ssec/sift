ssec/sift

commit 48a1c60bb2e64f88fe845394fca208d91478b02a

Fix issue with shell cmd call in watchdog.py
Since arbitrary messages including whitespaces and quotation marks are
passed to a shell command these must be properly quoted. Let
shlex.quote() do that job.
To have better control about what happens remove the placeholder
based configuration for the GMD raiseEvent.sh command and let MTG-SIFT
put the arguments in the right order. This comes at the price of
slightly tighter coupling of the MTG-SIFT implementation to the
raiseEvent.sh call signature.

The sample configuration watchdog.yaml must also be adopted to this
change.

uwsift/util/watchdog.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import shlex
 import subprocess
 from datetime import datetime
 from socket import gethostname
@@ -29,8 +30,9 @@ class Watchdog:
                                " in the watchdog config")
         self.heartbeat_file = heartbeat_file.replace("$$CACHE_DIR$$", cache_dir)
 
-        self.notification_cmd = config.get("watchdog.notification_cmd", None)
-        if self.notification_cmd is None:
+        notification_cmd = config.get("watchdog.notification_cmd", None)
+        self.notification_cmd = shlex.quote(notification_cmd)
+        if not self.notification_cmd:
             LOG.warning("Can't send notifications"
                         " because `notification_cmd` isn't configured")
 
@@ -64,13 +66,15 @@ class Watchdog:
         return datetime.fromtimestamp(timestamp)
 
     def _notify(self, level: int, text: str):
-        if self.notification_cmd is None:
+        if not self.notification_cmd:
             LOG.log(level, text)
         else:
-            cmd = self.notification_cmd.replace("$$MACHINE$$", self.hostname)
-            cmd = cmd.replace("$$PROCESS_NAME$$", f"{APPLICATION_DIR}-watchdog")
-            cmd = cmd.replace("$$SEVERITY$$", logging.getLevelName(level))
-            cmd = cmd.replace("$$TEXT$$", text)
+            machine = shlex.quote(self.hostname)
+            process_name = shlex.quote(f"{APPLICATION_DIR}-watchdog")
+            severity = shlex.quote(logging.getLevelName(level))
+            text = shlex.quote(text)
+            cmd = (f"{self.notification_cmd}"
+                   f" {machine} {process_name} {severity} {text}")
 
             try:
                 subprocess.run(cmd, shell=True, check=True)

resources/config/SIFT/settings/config/watchdog.yaml
@@ -1,6 +1,6 @@
 watchdog:
   heartbeat_file: "$$CACHE_DIR$$/heartbeat.txt"
-  notification_cmd: "/path/to/raiseEvent.sh $$MACHINE$$ $$PROCESS_NAME$$ $$SEVERITY$$ $$TEXT$$"
+  notification_cmd: /path/to/raiseEvent.sh
   # Unit of the following settings: seconds
   heartbeat_check_interval: 60
   max_tolerable_dataset_age: 600

commit bd0ee4213c9c9efb07c74d701d6d2529b142fb5a

Fix if expression being True always in watchdog.py
bool(shlex.quote(None)) == True

Therefore the warning is never logged.
Fixes: 48a1c60b

uwsift/util/watchdog.py
@@ -31,11 +31,13 @@ class Watchdog:
                                " in the watchdog config")
         self.heartbeat_file = heartbeat_file.replace("$$CACHE_DIR$$", cache_dir)
 
+        self.notification_cmd = None
         notification_cmd = config.get("watchdog.notification_cmd", None)
-        self.notification_cmd = shlex.quote(notification_cmd)
-        if not self.notification_cmd:
+        if not notification_cmd:
             LOG.warning("Can't send notifications"
                         " because `notification_cmd` isn't configured")
+        else:
+            self.notification_cmd = shlex.quote(notification_cmd)
 
         heartbeat_check_interval = config.get(
             "watchdog.heartbeat_check_interval", None)