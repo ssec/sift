5631b054616f91fe9138db0ad8690496c312f276
Alexander Rettig <alexander.rettig@askvisual.de>
2021-01-28 16:56:14 +0100

    Add default area definition 'longlat' as fallback
    Make sure, that at least one area definition for pseudo projection Plate
    Carree is available.
    It is checked, whether there is already a 'longlat' projection
    configured and only if not, a default Plate Carree world area definition
    with resolution 0.1 degrees is added to the available area definitions.
    
    Rationale:
    - If no area definition configuration is given MTG-SIFT cannot work if
      there is no fallback default area definition
    - To have a Plate Carree projection at hand any time is very helpful
      when examining new data where a suitable area definition may not be
      available yet.

--- uwsift/model/area_defnitions_manager.py
+++ uwsift/model/area_defnitions_manager.py
@@ -14,13 +14,36 @@ __docformat__ = 'reStructuredText'
 import logging
 import typing as typ
 
-from pyresample.area_config import AreaNotFound
+from pyresample.area_config import AreaNotFound, load_area_from_string
 from pyresample.geometry import AreaDefinition
 from satpy.resample import get_area_def
 
 from uwsift import config
 
 LOG = logging.getLogger(__name__)
+
+DEFAULT_AREAS = {
+    # A plate carree Projection (aka geographic projection, lat/lon projection,
+    # plane chart) should be available always. It will be only added though,
+    # if there is no other area definition found in configuration which uses the
+    # 'latlong' projection (or its PROJ.4 aliases)
+    'Plate Carree': 'plate_carree'
+}
+
+DEFAULT_AREA_DEFINITIONS_YAML = """
+plate_carree:
+  description:
+    Plate Carree with 0.1 degree resolution
+  projection:
+    proj: longlat
+    ellps: WGS84
+  shape:
+    height: 1800
+    width: 3600
+  area_extent:
+    lower_left_xy: [-180.0, -90.0]
+    upper_right_xy: [180.0, 90.0]
+"""
 
 
 class AreaDefinitionsManager:
@@ -49,6 +72,22 @@ class AreaDefinitionsManager:
 
             LOG.info(f"Adding area definition: {area_def_name} -> {area_id}")
             cls._available_area_defs[area_def_name] = area_def
+
+        # Check for existence of at least one 'latlong' projection
+        # (https://proj.org/operations/conversions/latlon.html)
+        for area_def in cls._available_area_defs.values():
+            proj: str = area_def.proj_dict["proj"]
+            if proj in ("latlon", "latlong", "lonlat", "longlat"):
+                print(f"FOUND {proj}")
+                return
+
+        # Add default area definition(s)?
+        for area_def_name, area_id in DEFAULT_AREAS.items():
+            area_def = \
+                load_area_from_string(DEFAULT_AREA_DEFINITIONS_YAML, area_id)
+
+        LOG.info(f"Adding default area definition: {area_def_name} -> {area_id}")
+        cls._available_area_defs[area_def_name] = area_def
 
     @classmethod
     def available_area_def_names(cls):
