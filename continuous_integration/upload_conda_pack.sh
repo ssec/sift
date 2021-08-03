074ca94ba2dfa4e009688840ab849c7b336af639
David Hoese <david.hoese@ssec.wisc.edu>
2021-08-03 15:40:49 -0500

    Re-add -k flag to curl commands host checking doesn't seem to work with SSEC FTP

--- continuous_integration/upload_conda_pack.sh
+++ continuous_integration/upload_conda_pack.sh
@@ -11,15 +11,11 @@ else
     odir="experimental/"
 fi
 # Upload the new bundle
-set +e
-ls -l $HOME/.ssh
-cat $HOME/.ssh/known_hosts
-set -e
-curl --ftp-create-dirs -T SIFT_*.*.*_*.* --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/${odir}
+curl -k --ftp-create-dirs -T SIFT_*.*.*_*.* --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/${odir}
 set +e
 # Delete any old
 if [[ $GIT_TAG =~ [0-9]+.[0-9]+.[0-9]+ ]]; then
-    curl -l --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/experimental/ | grep SIFT_*.*.*_*.* | xargs -I{} -- curl -v --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/experimental/ -Q "RM experimental/{}"
+    curl -k -l --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/experimental/ | grep SIFT_*.*.*_*.* | xargs -I{} -- curl -k -v --key $HOME/.ssh/id_rsa_sftp sftp://sift@ftp.ssec.wisc.edu/experimental/ -Q "RM experimental/{}"
     if [ $? -ne 0 ]; then
         echo "Failed to delete old experimental SIFT tarballs from FTP server"
     fi
