4a9038bee70e55457d5cd8fa0f0cf7648705d09a
Max Kreischer <max.kreischer@askvisual.de>
2022-01-27 14:29:06 +0100

    Add 'openMenu' method to LayerTreeView
    Also adds 'change_layer_colormap_menu' method, as this is needed in
    'openMenu'.
    The 'change_layer_colormap_menu' method was adopted from existing SIFT
    code but changed to use the new layer concept.

--- uwsift/view/layer_tree_view.py
+++ uwsift/view/layer_tree_view.py
@@ -1,8 +1,17 @@
+import logging
+from uuid import UUID
+
 from PyQt5.QtCore import QModelIndex, Qt
 from PyQt5.QtWidgets import QAbstractItemView, QHeaderView, QTreeView
+from PyQt5.QtWidgets import (QMenu)
 
+from uwsift.common import Kind
 from uwsift.common import LayerModelColumns as LMC  # noqa
+from uwsift.model.layer_item import LayerItem
 from uwsift.util.widgets.pie_dial import PieDialDelegate
+from uwsift.view.colormap_dialogs import ChangeColormapDialog
+
+LOG = logging.getLogger(__name__)
 
 
 class LayerTreeView(QTreeView):
@@ -30,7 +39,7 @@ class LayerTreeView(QTreeView):
                                       self.visibility_delegate)
         self.setEditTriggers(QAbstractItemView.AllEditTriggers)
 
-        self.customContextMenuRequested.connect(self.openMenu)
+        self.customContextMenuRequested.connect(self.open_layer_context_menu)
 
     def resizeColumnsToContents(self) -> None:  # noqa
         """Resize all columns to their current contents of the model."""
@@ -54,3 +63,44 @@ class LayerTreeView(QTreeView):
 
             if not self.isPersistentEditorOpen(model_idx):
                 self.openPersistentEditor(model_idx)
+
+    def open_layer_context_menu(self, position):
+
+        menu = QMenu()
+        selection_model_idx = self.selectionModel().currentIndex()
+
+        model_idx = self.model().index(selection_model_idx.row(), 0,
+                                       QModelIndex())
+        if not model_idx.isValid():
+            raise ValueError(f"Entry at row {selection_model_idx.row()}"
+                             f" not in model")
+
+        layer: LayerItem = self.model().layers[model_idx.row()]
+        actions = {}
+        if layer is not None and layer.kind in [Kind.IMAGE,
+                                                Kind.COMPOSITE,
+                                                Kind.CONTOUR]:
+            actions.update(self.change_layer_colormap_menu(menu, layer.uuid))
+
+        if not actions:
+            action = menu.addAction("No actions available for this layer")
+            action.setEnabled(False)
+
+        sel = menu.exec_(self.mapToGlobal(position))
+        if sel is None:
+            return
+        elif sel in actions:
+            return actions[sel](sel)
+        else:
+            LOG.debug("Unimplemented menu option '{}'".format(sel.text()))
+
+    def change_layer_colormap_menu(self, menu: QMenu, selected_uuid: UUID):
+        model = self.model()
+        def _show_change_colormap_dialog(action): # noqa
+            d = ChangeColormapDialog(model, selected_uuid, parent=self)
+            d.show()
+            d.raise_()
+            d.activateWindow()
+
+        action = menu.addAction('Change Colormap...')
+        return {action: _show_change_colormap_dialog}
