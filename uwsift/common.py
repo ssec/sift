6f4984cdc8fb7040b3c5f1b51ac951c868002f4f
Alexander Rettig <alexander.rettig@askvisual.de>
2022-02-01 19:32:18 +0100

    Ensure sensible default values for 'Presentation'
    for all its fields except
    - uuid
    - kind
    
    Setting default values facilitates the provision of 'Presentation'
    objects for the creation of layers, especially in the case of system
    layers with generated data.

--- uwsift/common.py
+++ uwsift/common.py
@@ -385,16 +385,21 @@ class Presentation:
     z_order comes from the layerset
 
     """
-    uuid: UUID  # dataset in the document/workspace
-    kind: Kind  # what kind of layer it is
-    visible: bool  # whether it's visible or not
-    a_order: int  # None for non-animating, 0..n-1 what order to draw in during animation
-    colormap: object  # name or uuid: color map to use; name for default, uuid for user-specified
-    style: object  # name or uuid: SVG/HTML style to use; name for default, (FUTURE?) uuid for user-specified
-    climits: tuple  # valid min and valid max used for color mapping normalization
-    gamma: float  # valid (0 to 5) for gamma correction (default should be 1.0)
-    mixing: object  # mixing mode constant
-    opacity: float
+    uuid: Optional[UUID]     # dataset in the document/workspace, None if
+                             # referring to a system layer
+    kind: Kind               # what kind of layer it is
+    visible: bool = True     # whether it's visible or not
+    a_order: int = None      # None for non-animating, 0..n-1 what order to draw
+                             # in during animation
+    colormap: object = None  # name or uuid: color map to use; name for default,
+                             # uuid for user-specified
+    style: object = None     # name or uuid: SVG/HTML style to use; name for
+                             # default, (FUTURE?) uuid for user-specified
+    climits: tuple = None    # valid min and valid max used for color mapping
+                             # normalization
+    gamma: float = 1.0       # valid (0 to 5) for gamma correction
+    mixing: object = None    # mixing mode constant
+    opacity: float = 1.0
 
 
 class ZList(MutableSequence):
