1118dc608107212be55860a7891f2d8d3dcc8cfc
Alexander Rettig <alexander.rettig@askvisual.de>
2022-04-11 12:04:43 +0200

    Revert "Show timeline even in auto update mode (for devel only)"
    Showing the timeline also in Auto-Update mode was intentionally ony done
    for development to see, which timesteps are currently in the LayerModel.
    
    At at least as it was done it is not suitable for end users and it is
    not an agreed feature yet.
    
    TODO: Consider showing the timeline in Auto-Update mode as well, see #137.
    
    This reverts commit 7096a89b4fecea12fc5b5d0cdb1aceba818c9b67.

--- uwsift/__main__.py
+++ uwsift/__main__.py
@@ -876,7 +876,7 @@ class Main(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         if AUTO_UPDATE_MODE__ACTIVE:
             self.ui.animFrame.hide()
-            # self.ui.timelineFrame.hide()
+            self.ui.timelineFrame.hide()
             self.ui.progressBar.hide()
             self.ui.progressText.hide()
 
