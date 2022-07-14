558fc503cb029d2f3e67add80e43e822da5dbdca
andream <andrea.meraner@eumetsat.int>
2022-07-14 15:28:19 +0200

    update create_distribution.sh and update_setting.py to use default configs from resources

--- etc/create_distribution.sh
+++ etc/create_distribution.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+cwd=$(pwd)
+
 HOME_PYINSTALLER_BUILD="../../pyinstaller/onedir/"
 DIST_OUTPUT="../../distribution-build"
 
@@ -9,8 +11,11 @@ SCR_TEMPLATE="./mtgsift-dist-template"
 rm -Rf $DIST_OUTPUT/
 mkdir -p $DIST_OUTPUT
 
+
 #copy the dist template
 cp -R "$SCR_TEMPLATE" "$DIST_OUTPUT/mtgsift-dist"
+#copy the default settings
+cp -R "../resources" "$DIST_OUTPUT/mtgsift-dist"
 
 #copy the built pyinstaller version as lib in the dist
 cp -R "$HOME_PYINSTALLER_BUILD/dist/mtgsift" "$DIST_OUTPUT/mtgsift-dist/lib"
@@ -29,3 +34,11 @@ ln -fs ../resources/qtwebengine_resources_100p.pak qtwebengine_resources_100p.pa
 ln -fs ../resources/qtwebengine_resources_200p.pak qtwebengine_resources_200p.pak
 ln -fs ../resources/qtwebengine_resources.pak qtwebengine_resources.pak
 ln -fs ../translations/qtwebengine_locales qtwebengine_locales
+cd $cwd
+
+# copy .h files from gribapi (make these lines work https://github.com/ecmwf/eccodes-python/blob/develop/gribapi/bindings.py#L41-L42)
+mkdir -p "$DIST_OUTPUT/mtgsift-dist/lib/gribapi/"
+cp "$CONDA_PREFIX/lib/python3.9/site-packages/gribapi/grib_api.h" "$DIST_OUTPUT/mtgsift-dist/lib/gribapi/"
+cp "$CONDA_PREFIX/lib/python3.9/site-packages/gribapi/eccodes.h" "$DIST_OUTPUT/mtgsift-dist/lib/gribapi/"
+
+set +x
