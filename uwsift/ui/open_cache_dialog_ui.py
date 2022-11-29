71115419a0abc65d1fa468a19f0bf7ac05d91a5f
Alexander Rettig <alexander.rettig@askvisual.de>
2022-11-29 14:40:00 +0100

    Update the *_ui.py files to PyQt 5.15.7
    ... so that all these files are once again consistently generated with
    the same PyUIC.
    
    Zeilen,

--- uwsift/ui/open_cache_dialog_ui.py
+++ uwsift/ui/open_cache_dialog_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/open_cache_dialog.ui'
+# Form implementation generated from reading ui file 'open_cache_dialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.7
 #
