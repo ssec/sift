1bc5ae13d0f672b2a60a77e7bff0ac22f6a2f298
Alexandra Melzer <Alexandra.Melzer@askvisual.de>
2022-12-13 16:06:13 +0100

    Resolve mypy issues in 'time_matcher.py'
    Get rid of
    
    uwsift/control/time_matcher.py:9: error: Incompatible default for argument "policy" (default has type "None", argument has type "Callable[..., Any]")  [assignment]
    uwsift/control/time_matcher.py:9: note: PEP 484 prohibits implicit Optional. Accordingly, mypy has changed its default to no_implicit_optional=True
    uwsift/control/time_matcher.py:9: note: Use https://github.com/hauntsaninja/no_implicit_optional to automatically upgrade your codebase
    
    by fine tuning type annotations

--- uwsift/control/time_matcher.py
+++ uwsift/control/time_matcher.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from typing import Callable, List
+from typing import Callable, List, Optional
 
 
 class TimeMatcher:
     def __init__(self, policy: Callable):
         self.policy = policy
 
-    def match(self, timeline: List[datetime], t_sim: datetime, policy: Callable = None):
+    def match(self, timeline: List[datetime], t_sim: datetime, policy: Optional[Callable] = None):
         if not isinstance(t_sim, datetime):
             raise ValueError("Queried Time not an instance of a datetime object.")
         if policy is None:
