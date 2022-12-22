ssec/sift

commit 0e35301d7471d0f60371f9282ddde3869a466fb8

Enable tree navigation in "Select Data Directory" dialog
... which opens, when a directory is to be chosen with the
OpenFileWizard.
By the way, change the dialog title from "Select folder to open" by
"Select Data Directory".

uwsift/view/open_file_wizard.py
@@ -338,9 +338,12 @@ class OpenFileWizard(QtWidgets.QWizard):
 
     def _open_select_folder_dialog(self):
         """Show folder chooser and update table with files matching the filter pattern."""
-        file_dialog = QtWidgets.QFileDialog(self, "Select folder to open")
+        file_dialog = QtWidgets.QFileDialog(self, "Select Data Directory")
         file_dialog.setFileMode(QtWidgets.QFileDialog.Directory)
         file_dialog.setOption(QtWidgets.QFileDialog.ShowDirsOnly, True)  # Must come after setFileMode()
+        tree = file_dialog.findChild(QtWidgets.QTreeView)
+        tree.setRootIsDecorated(True)
+        tree.setItemsExpandable(True)
 
         if self._initial_directory:
             file_dialog.setDirectory(self._initial_directory)

commit 136c960f80c896e00cb5bce7cc6d852147a2b782

Fix OpenFileWizard Folder Selection fail on Windows
The customization of the file dialog as introduced with commit

0e35301d ("Enable tree navigation in "Select Data Directory" dialog", 2022-12-07)

can only work for Qt based file dialogs. But since Qt prefers to
instantiate the platform native file dialog via Qt's QFileDialog
interface this customization must only be applied,
if the actual file dialog provides a QTreeWidget for the files/directory
view.

The customiziation may only be applicable on KDE or other Qt based
Windows Managers on Linux, not on Windows and (not tested, but most
likely) not on MacOS. Also on GTK based Windows Managers (e.g. Gnome) it
may not be available.

Fixes #161

uwsift/view/open_file_wizard.py
@@ -349,9 +349,16 @@ class OpenFileWizard(QtWidgets.QWizard):
             self.file_dialog = QtWidgets.QFileDialog(self, "Select Data Directory")
             self.file_dialog.setFileMode(QtWidgets.QFileDialog.Directory)
             self.file_dialog.setOption(QtWidgets.QFileDialog.ShowDirsOnly, True)  # Must come after setFileMode()
+
+            # Try enabling directory tree navigation in the file dialog.
+            # Qt prefers to use the platform native file dialog. If this dialog
+            # is not based on Qt (e.g. on Windows), the next call returns None,
+            # then we have to live with what the file dialog of the platform
+            # provides.
             tree = self.file_dialog.findChild(QtWidgets.QTreeView)
-            tree.setRootIsDecorated(True)
-            tree.setItemsExpandable(True)
+            if tree:
+                tree.setRootIsDecorated(True)
+                tree.setItemsExpandable(True)
 
             if self._initial_directory:
                 self.file_dialog.setDirectory(self._initial_directory)