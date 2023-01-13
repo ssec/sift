ssec/sift

commit 28dbb863588620dcc6b8a6ef08e14c78495c5785

Show the point probe information of the selected layer
... and no longer that of the top layer.

When the selection changes, update the probe information.
If no layer is selected, behave as if a non-probeable layer is selected.

Closes #122

uwsift/__main__.py
@@ -428,10 +428,11 @@ class Main(QtWidgets.QMainWindow):
 
     def update_point_probe_text(self, probe_name, state=None, xy_pos=None, uuid=None, animating=None):
         if uuid is None:
-            (
-                top_probeable_layer,
-                product_dataset,
-            ) = self.layer_model.get_top_probeable_layer_with_active_product_dataset()
+            current_row = self.ui.treeView.currentIndex().row()
+            product_dataset = None
+            if current_row >= 0:
+                selected_probeable_layer = self.layer_model.layers[current_row]
+                product_dataset = selected_probeable_layer.get_first_active_product_dataset()
             uuid = None if product_dataset is None else product_dataset.uuid
         if state is None or xy_pos is None:
             _state, _xy_pos = self.graphManager.current_point_probe_status(probe_name)
@@ -466,7 +467,7 @@ class Main(QtWidgets.QMainWindow):
                 data_str = "N/A"
                 layer_str = "N/A"
             else:
-                info = top_probeable_layer.info
+                info = selected_probeable_layer.info
                 unit_info = info[Info.UNIT_CONVERSION]
                 data_point = unit_info[1](data_point)
                 data_str = unit_info[2](data_point, numeric=False)
@@ -761,6 +762,8 @@ class Main(QtWidgets.QMainWindow):
         self.layer_model.didUpdateLayers.connect(self.graphManager.update_point_probe)
         self.layer_model.didUpdateLayers.connect(self.graphManager.handleActiveProductDatasetsChanged)
         self.layer_model.didChangeRecipeLayerNames.connect(self.graphManager.handleActiveProductDatasetsChanged)
+
+        self.ui.treeView.selectedLayerForProbeChanged.connect(self.update_point_probe_text)
 
         # Connect to an unnamed slot (lambda: ...) to strip off the argument
         # (of type dict) from the signal 'didMatchTimes'

uwsift/view/layer_tree_view.py
@@ -10,12 +10,14 @@ from uwsift.common import Platform
 from uwsift.model.layer_item import LayerItem
 from uwsift.util.widgets.pie_dial import PieDialDelegate
 from uwsift.view.colormap_dialogs import ChangeColormapDialog
+from uwsift.view.probes import DEFAULT_POINT_PROBE
 
 LOG = logging.getLogger(__name__)
 
 
 class LayerTreeView(QTreeView):
     layerSelectionChanged = pyqtSignal(tuple)
+    selectedLayerForProbeChanged = pyqtSignal(str)
 
     def __init__(self, *args, **kwargs):
         super(LayerTreeView, self).__init__(*args, **kwargs)
@@ -124,6 +126,7 @@ class LayerTreeView(QTreeView):
         if len(self.model().layers) > 0:
             selected_layer: tuple = (self.model().layers[current.row()],)
             self.layerSelectionChanged.emit(selected_layer)
+            self.selectedLayerForProbeChanged.emit(DEFAULT_POINT_PROBE)
 
     def begin_layers_removal(self, *args, **kwargs):
         """

commit 4d3182b5b48a64549d304f772f0710aec4f6cc6c

Fix stuck point probe text in status line
To achieve this, revise the updating of the point probe text.  There is
a problem when selecting a layer that has no point probe value, e.g.
"MC_IMAGE" ([M]ulti [C]hannel Image).
This problem was introduced with 28dbb863
("Show the point probe information of the selected layer", 2022-09-26).

Make it possible to select any type of layer again without errors when
the point probe is active/set.

Fixes #163

Additionally:
- When a probe is active always display the name of the selected layer
  in the status bar, even if everything else evalutates to N/A
- Implementation detail: make the implemetation of the processing of the
  point probe value to the unit converted and formatted display string
  more verbose in order to make it more comprehensible.

uwsift/__main__.py
@@ -64,7 +64,7 @@ from uwsift.util import (
     check_imageio_deps,
     get_package_data_dir,
 )
-from uwsift.util.common import format_wavelength, normalize_longitude
+from uwsift.util.common import normalize_longitude
 from uwsift.util.logger import configure_loggers
 from uwsift.view.algebraic_config import AlgebraicLayerConfigPane
 from uwsift.view.colormap_editor import ColormapEditor
@@ -428,6 +428,8 @@ class Main(QtWidgets.QMainWindow):
         if current_row >= 0:
             selected_layer = self.layer_model.layers[current_row]
             product_dataset = selected_layer.get_first_active_product_dataset()
+        else:
+            selected_layer = None
         uuid = None if product_dataset is None else product_dataset.uuid
 
         state, xy_pos = self.graphManager.current_point_probe_status(probe_name)
@@ -445,27 +447,22 @@ class Main(QtWidgets.QMainWindow):
         data_str = "N/A"
         layer_str = "N/A"
 
-        if state and uuid is not None:
-            try:
-                data_point = self.workspace.get_content_point(uuid, xy_pos)
-                col, row = self.workspace.position_to_grid_index(uuid, xy_pos)
-            except ValueError:
-                LOG.debug("Could not get data value", exc_info=True)
-                data_point = None
+        if state:
+            if selected_layer:
+                layer_str = selected_layer.short_descriptor
 
-            if data_point is not None:
-                info = selected_layer.info
-                unit_info = info[Info.UNIT_CONVERSION]
-                data_point = unit_info[1](data_point)
-                data_str = unit_info[2](data_point, numeric=False)
-                if info.get(Info.CENTRAL_WAVELENGTH):
-                    wl_str = format_wavelength(info[Info.CENTRAL_WAVELENGTH])
-                    layer_str = "{}, {}".format(info[Info.SHORT_NAME], wl_str)
-                else:
-                    layer_str = info[Info.SHORT_NAME]
+            if uuid is not None:
+                data_point = selected_layer.probe_value
+
+                if data_point is not None:
+                    unit_info = selected_layer.info[Info.UNIT_CONVERSION]
+                    convert_unit_func = unit_info[1]
+                    format_quantity_func = unit_info[2]
+                    data_str = format_quantity_func(convert_unit_func(data_point), numeric=False)
+                    col, row = self.workspace.position_to_grid_index(uuid, xy_pos)
 
         self.ui.cursorProbeLayer.setText(layer_str)
-        self.ui.cursorProbeText.setText("{} ({}) [{}, {}]".format(data_str, probe_loc, col, row))
+        self.ui.cursorProbeText.setText(f"{data_str} ({probe_loc}) [{col}, {row}]")
 
     @staticmethod
     def run_gc_after_layer_deletion():
@@ -707,6 +704,7 @@ class Main(QtWidgets.QMainWindow):
         self.layer_model.didChangeRecipeLayerNames.connect(self.graphManager.handleActiveProductDatasetsChanged)
 
         self.ui.treeView.selectedLayerForProbeChanged.connect(self.update_point_probe_text)
+        self.layer_model.didChangeRecipeLayerNames.connect(self.update_point_probe_text)
 
         # Connect to an unnamed slot (lambda: ...) to strip off the argument
         # (of type dict) from the signal 'didMatchTimes'

uwsift/model/layer_model.py
@@ -46,7 +46,7 @@ class LayerModel(QAbstractItemModel):
     didChangeGamma = pyqtSignal(dict)
     didChangeLayerVisible = pyqtSignal(UUID, bool)
     didChangeLayerOpacity = pyqtSignal(UUID, float)
-    didChangeRecipeLayerNames = pyqtSignal()
+    didChangeRecipeLayerNames = pyqtSignal(str)
 
     didUpdateLayers = pyqtSignal()
     didReorderLayers = pyqtSignal(list)
@@ -114,7 +114,13 @@ class LayerModel(QAbstractItemModel):
         # The minimal 'dataset' information required by LayerItem
         # initialization:
         pseudo_info = frozendict(
-            {Info.KIND: Kind.LINES, Info.PLATFORM: Platform.SYSTEM, Info.INSTRUMENT: Instrument.GENERATED, "name": name}
+            {
+                Info.KIND: Kind.LINES,
+                Info.PLATFORM: Platform.SYSTEM,
+                Info.INSTRUMENT: Instrument.GENERATED,
+                Info.SHORT_NAME: name,
+                "name": name,
+            }
         )
 
         presentation = Presentation(uuid=None, kind=Kind.LINES)
@@ -775,9 +781,11 @@ class LayerModel(QAbstractItemModel):
         self.didRequestRGBCompositeRecipeCreation.emit(layers)
 
     def update_recipe_layer_name(self, recipe: Recipe):
+        from uwsift.view.probes import DEFAULT_POINT_PROBE
+
         recipe_layer: LayerItem = self._get_layer_of_recipe(recipe.id)
         recipe_layer.update_invariable_display_data()
-        self.didChangeRecipeLayerNames.emit()
+        self.didChangeRecipeLayerNames.emit(DEFAULT_POINT_PROBE)
 
         index = self.index(recipe_layer.order, LMC.NAME)
         self.dataChanged.emit(index, index)