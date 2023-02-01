ssec/sift

commit c0fcfd63203bd99038670713b1849e2629b3a49e

Add experimental pyinstaller splash screen with dummy splash image

uwsift/__main__.py
@@ -1460,5 +1460,24 @@ def main() -> int:
     return 0
 
 
+def close_splash_screen():
+    """
+    Helper function to close startup splash screen provided by pyinstaller.
+    If pyinstaller package has no splash screen or this is not started as
+    pyinstaller package at all the function does nothing.
+    """
+    if "_PYIBoot_SPLASH" in os.environ:
+        try:
+            import pyi_splash
+
+            pyi_splash.close()
+        except:
+            # not good not terrible. Splash screen might be still open but
+            # more likely there was no splash screen to begin with and the
+            # environment variable set defined by something else.
+            pass
+
+
 if __name__ == "__main__":
+    close_splash_screen()
     sys.exit(main())

mtgsift-pyinstaller-package.spec
@@ -175,7 +175,12 @@ pyz = PYZ(a.pure,
 # options = [ ('v', None, 'OPTION') ]
 options=[]
 
+splash = Splash('splash.png',
+                binaries=a.binaries,
+                datas=a.datas)
+
 exe = EXE(pyz,
+          splash,
           a.scripts,
           options,
           exclude_binaries=True,
@@ -186,6 +191,7 @@ exe = EXE(pyz,
           console=True )
 
 coll = COLLECT(exe,
+               splash.binaries,
                a.binaries,
                a.zipfiles,
                a.datas,

commit d524d8a40f5928b4620e7a5b8485d02092ea344c

Resolve "new" bandit issue in '__main__.py'
Get rid of

- [B110:try_except_pass] Try, Except, Pass detected.
  Location: uwsift/__main__.py:1478:8

This issue was introduced with the commit

c0fcfd63 ("Add experimental pyinstaller splash screen with dummy splash image", 2022-11-02)

which was developed before 'bandit' pre-commit was "armed".

uwsift/__main__.py
@@ -1475,7 +1475,7 @@ def close_splash_screen():
             import pyi_splash
 
             pyi_splash.close()
-        except:
+        except ModuleNotFoundError:
             # not good not terrible. Splash screen might be still open but
             # more likely there was no splash screen to begin with and the
             # environment variable set defined by something else.