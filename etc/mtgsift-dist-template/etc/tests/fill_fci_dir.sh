5721feba8cd78ad3632c8b49e272f2a638e89669
andream <andrea.meraner@eumetsat.int>
2023-05-10 12:50:44 +0200

    remove deprecated mtgsift-dist-template, including fill_dir codes. setting updates codes are not needed anymore with new default configs mechanism

--- etc/mtgsift-dist-template/etc/tests/fill_fci_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT_DIR=/opt/sift/Data/MTG-Incoming
-echo "Clean output dir"
-rm -Rf $OUTPUT_DIR/*.nc
-
-/opt/sift/mtgsift-0.8/etc/tests/py-tests/bin/python3 fill_dir_periodically.py -i /tcenas/proj/optcalimg/test_data/SIFT/fci_l1c_fdhsi/26h_dataset/20200404_RC139 -o /opt/mtg-sift/Data/MTG-Incoming -t 20
