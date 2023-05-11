cf2fb303f52627653edc0e87dfdcc352d009066f
andream <andrea.meraner@eumetsat.int>
2023-05-11 19:37:27 +0200

    move exclude_datasets from data_reading to open_file_wizard, reinstate default_reader.yaml

--- uwsift/workspace/importer.py
+++ uwsift/workspace/importer.py
@@ -164,7 +164,7 @@ def filter_dataset_ids(ids_to_filter: Iterable[DataID]) -> Generator[DataID, Non
     """Generate only non-filtered DataIDs based on EXCLUDE_DATASETS global filters."""
     # skip certain DataIDs
     for ds_id in ids_to_filter:
-        for filter_key, filtered_values in config.get("data_reading.exclude_datasets").items():
+        for filter_key, filtered_values in config.get("open_file_wizard.exclude_datasets").items():
             if filtered_values and get_id_value(ds_id, filter_key) in filtered_values:
                 break
         else:
