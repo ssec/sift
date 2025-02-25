13882cda307f97311b0c93d5b0c03e03532368b8
Andrea Meraner <49722768+ameraner@users.noreply.github.com>
2025-02-25 19:59:02 +0100

    fix SEVIRI typo

--- uwsift/model/area_definitions_manager.py
+++ uwsift/model/area_definitions_manager.py
@@ -128,7 +128,7 @@ class AreaDefinitionsManager:
     def available_area_def_group_resolutions(cls, group_name):
         return cls._available_area_defs_group_by_group_name.get(group_name).keys()
 
-    # returns area group by its name --- for example if a group_name = MSG SEVERI FES, a return value will be
+    # returns area group by its name --- for example if a group_name = MSG SEVIRI FES, a return value will be
     # {3 km: msg_seviri_fes_3km, 1 km: msg_seviri_fes_1km}
     @classmethod
     def area_group_by_group_name(cls, group_name):
